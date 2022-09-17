1a43ee80e8f01fc3d3b1487022acb912d8309884
neavr <mazatlum.mv@gmail.com>
2022-09-17 13:57:02 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+  
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\LoginController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group that
+| contains the "web" middleware group. Now create something great!
+|
+*/
+  
+Route::resource('products', ProductController::class);
+Route::resource('login', CustomAuthController::class);
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
